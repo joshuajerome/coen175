ba6b0c916a533a9ca0fb7af4562b70b893eb7922
Joshua Jerome <joshua.jerome@gmail.com>
2023-05-22 16:43:44 -0700

    added new examples

--- project5/examples/decl.c
+++
@@ -1,9 +0,0 @@
-int f1()[10];			/* 'f1' has invalid type */
-int (*f2())[10];
-int (*f3())[10][20];
-
-int a1[10]();			/* 'a1' has invalid type */
-int (*a2[10])();
-int (**a3[10])();
-
-int a[10][20];
